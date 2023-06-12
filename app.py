4f55719796cf4a99740b8e6cc5fafeb1b2e182cd
ingrid-oguro <104454935+ingrid-oguro@users.noreply.github.com>
2023-06-12 14:40:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ usernames = ["marcelod","simone.cruz","jose.campos",
 
 # load hashed passwords
 
-file_path = Path(__file__).parent / "hashed.pw.pkl"
+#file_path = Path(__file__).parent / "hashed.pw.pkl"
 #file_path = Path("/content/drive/MyDrive/Relatorios_20231/untitled/hashed_pw.pkl")
-#file_path = Path("hashed_pw.pkl")
+file_path = Path("hashed_pw.pkl")
 with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
 
